45ec89028ef20007ba50145a0020c61ada3d42f1
JNightcrawler <37281948+JNightcrawler@users.noreply.github.com>
2020-09-13 17:38:56 +0530

    update

--- src/app/tablesort.pipe.ts
+++ src/app/tablesort.pipe.ts
@@ -1,5 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { searchall } from './shared/searchAll';
+import { searchall } from './shared/searchall';
 
 @Pipe({
   name: 'tablesort'
